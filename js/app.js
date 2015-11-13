96bd9cfeb1bcc28994b6dbcf89388bae8f0f05c3
Jules Bonnard <julesbonnard@gmail.com>
2015-11-13 12:37:18 +0100

    add google analytics

--- js/app.js
+++ js/app.js
@@ -589,3 +589,25 @@ var onCopyrightChange = function() {
 }
 
 $(onDocumentLoad);
+
+/************
+  Google Analytics
+  ************/
+
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-64253904-4', {
+  'alwaysSendReferrer': true
+});
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview');
